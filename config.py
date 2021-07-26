84bd57650dd394dd756fc9921bc659d2978d9679
git22abhishek <abhishekpwahane@gmail.com>
2021-07-26 13:47:27 +0530

    Fixed config

--- config.py
+++ config.py
@@ -1,78 +1,95 @@
 class Data:
-    DATASET_ROOT_DIR = '../datasets/IAM'
-    CSV_FILE_PATH = '../datasets/IAM/IAM_df.csv'
-    SHUFFLE = True
-    SEED = 42
-    NUM_WORKERS = 0
-    DEFAULT_PARTITION = False
-    PARTITION = (0.6, 0.2, 0.2)
-    BATCH_SIZE = (32, 16, 16)
 
-    def _asdict():
+    def __init__(self) -> None:
+
+        self.DATASET_ROOT_DIR = '../datasets/IAM'
+        self.CSV_FILE_PATH = '../datasets/IAM/IAM_df.csv'
+        self.SHUFFLE = True
+        self.SEED = 42
+        self.NUM_WORKERS = 0
+        self.DEFAULT_PARTITION = False
+        self.PARTITION = (0.6, 0.2, 0.2)
+        self.BATCH_SIZE = (32, 16, 16)
+
+    def _asdict(self):
+
         return {
-            'DATASET_ROOT_DIR': Data.DATASET_ROOT_DIR,
-            'CSV_FILE_PATH': Data.CSV_FILE_PATH,
-            'SHUFFLE': Data.SHUFFLE,
-            'SEED': Data.SEED,
-            'NUM_WORKERS': Data.NUM_WORKERS,
-            'DEFAULT_PARTITION': Data.DEFAULT_PARTITION,
-            'PARTITION': Data.PARTITION,
-            'BATCH_SIZE': Data.BATCH_SIZE
+            'DATASET_ROOT_DIR': self.DATASET_ROOT_DIR,
+            'CSV_FILE_PATH': self.CSV_FILE_PATH,
+            'SHUFFLE': self.SHUFFLE,
+            'SEED': self.SEED,
+            'NUM_WORKERS': self.NUM_WORKERS,
+            'DEFAULT_PARTITION': self.DEFAULT_PARTITION,
+            'PARTITION': self.PARTITION,
+            'BATCH_SIZE': self.BATCH_SIZE
         }
 
 
 class Optimizer:
-    LR = 0.003
 
-    def _asdict():
-        return {'LR': Optimizer.LR}
+    def __init__(self) -> None:
+        self.LR = 0.003
+
+    def _asdict(self):
+        return {'LR': self.LR}
 
 
 class Scheduler:
-    FACTOR = 0.1
-    PATIENCE = 5
-    VERBOSE = True
 
-    def _asdict():
+    def __init__(self) -> None:
+
+        self.FACTOR = 0.1
+        self.PATIENCE = 5
+        self.VERBOSE = True
+
+    def _asdict(self):
+
         return {
-            'FACTOR': Scheduler.FACTOR,
-            'PATIENCE': Scheduler.PATIENCE,
-            'VERBOSE': Scheduler.VERBOSE,
-
+            'FACTOR': self.FACTOR,
+            'PATIENCE': self.PATIENCE,
+            'VERBOSE': self.VERBOSE,
         }
 
 
 class CTCLoss:
-    BLANK = 0
-    REDUCTION = 'sum'
-    ZERO_INFINITY = True
 
-    def _asdict():
+    def __init__(self) -> None:
+
+        self.BLANK = 0
+        self.REDUCTION = 'sum'
+        self.ZERO_INFINITY = True
+
+    def _asdict(self):
+
         return {
-            'BLANK': CTCLoss.BLANK,
-            'REDUCTION': CTCLoss.REDUCTION,
-            'ZERO_INFINITY': CTCLoss.ZERO_INFINITY,
+            'BLANK': self.BLANK,
+            'REDUCTION': self.REDUCTION,
+            'ZERO_INFINITY': self.ZERO_INFINITY,
         }
 
 
-class config:
-    data = Data()
-    opt = Optimizer()
-    sch = Scheduler()
-    ctc_loss = CTCLoss()
-    TIME_STEPS = 100
-    NUM_EPOCHS = 20
-    RESUME_TRAINING = False
-    TRAIN_CHECKPOINT_PATH = '../checkpoints/training_state.pth'
+class Config:
 
-    def _asdict():
+    def __init__(self) -> None:
+
+        self.data = Data()
+        self.opt = Optimizer()
+        self.sch = Scheduler()
+        self.ctc_loss = CTCLoss()
+        self.TIME_STEPS = 100
+        self.NUM_EPOCHS = 20
+        self.RESUME_TRAINING = False
+        self.TRAIN_CHECKPOINT_PATH = '../checkpoints/training_state.pth'
+
+    def _asdict(self):
+
         return {
-            'data': Data._asdict(),
-            'opt': Optimizer._asdict(),
-            'sch': Scheduler._asdict(),
-            'ctc_loss': CTCLoss._asdict(),
-            'TIME_STEPS': config.TIME_STEPS,
-            'NUM_EPOCHS': config.NUM_EPOCHS,
-            'RESUME_TRAINING': config.RESUME_TRAINING,
-            'TRAIN_CHECKPOINT_PATH': config.TRAIN_CHECKPOINT_PATH,
+            'data': self.data._asdict(),
+            'opt': self.opt._asdict(),
+            'sch': self.sch._asdict(),
+            'ctc_loss': self.ctc_loss._asdict(),
+            'TIME_STEPS': self.TIME_STEPS,
+            'NUM_EPOCHS': self.NUM_EPOCHS,
+            'RESUME_TRAINING': self.RESUME_TRAINING,
+            'TRAIN_CHECKPOINT_PATH': self.TRAIN_CHECKPOINT_PATH,
         }
